d2abf35a4aa24f4348bef45f9a15037cc7e8f6da
ginike <obi6065@gmail.com>
2019-08-30 14:47:02 -0800

    deleted url patterns

--- new_project/urls.py
+++ new_project/urls.py
@@ -19,8 +19,6 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
